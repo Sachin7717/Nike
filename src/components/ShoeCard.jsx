bb46699019a233d5c46f2b1792c60d93c7cc9d59
Sachin7717 <sachindhimann71@gmail.com>
2024-07-16 20:02:19 +0530

    shoecard completed

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,18 +1,15 @@
 
-const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImage }) => {
-  
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
     const handleClick = () => {
-      if (bigShoeImage !== imgUrl.bigShoe) {
-        changeBigShoeImage(imgUrl.bigShoe);
+      if (bigShoeImg !== imgURL.bigShoe) {
+        changeBigShoeImage(imgURL.bigShoe);
       }
     };
-  
-  
   
     return (
       <div
         className={`border-2 rounded-xl ${
-          bigShoeImage === imgUrl.bigShoe
+          bigShoeImg === imgURL.bigShoe
             ? "border-coral-red"
             : "border-transparent"
         } cursor-pointer max-sm:flex-1`}
@@ -20,7 +17,7 @@ const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImage }) => {
       >
         <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
           <img
-            src={imgUrl.thumbnail}
+            src={imgURL.thumbnail}
             alt='shoe colletion'
             width={130}
             height={105}
@@ -28,7 +25,6 @@ const ShoeCard = ({ imgUrl, changeBigShoeImage, bigShoeImage }) => {
           />
         </div>
       </div>
-      
     );
   };
   
