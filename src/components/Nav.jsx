0c85342dfc9847b1a046784bc6fe357c3a450afe
Sachin7717 <sachindhimann71@gmail.com>
2024-10-13 17:49:11 +0530

    changed name to vesti

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,9 +6,9 @@ const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
         <nav className='flex justify-center items-center  max-container '>
-            <a href="/">
+            {/* <a href="/">
             <img src={headerLogo} alt="Logo"  width={130} height={30} /> 
-            </a>
+            </a> */}
 
             <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
                 {navLinks.map((item) =>(
